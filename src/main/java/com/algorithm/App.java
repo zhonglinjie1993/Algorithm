ca278712bf4ac3367260b314456df5225cdab1d3
zhonglinjie <zhonglinjie@hotmail.com>
2018-03-28 21:26:39 +0800

    第一次提交

---
+++ src/main/java/com/algorithm/App.java
@@ -0,0 +1,13 @@
+package com.algorithm;
+
+import edu.princeton.cs.algs4.StdIn;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println ("Hello World!");
+
+    }
+}
